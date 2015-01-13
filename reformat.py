7355afdb5655eaad857fbc9a1366e59647a45617
Bobby Powers <bobbypowers@gmail.com>
2015-01-13 10:00:59 -0500

    delta too

--- reformat.py
+++ reformat.py
@@ -13,10 +13,17 @@ def main():
         if series[i][1] != 0:
             start = i-1
             break
-    time_off = series[start][0]
-    print 'time,volume'
-    for t, v in series[start:]:
-        print '%s\t%s' % (t - time_off, v)
+    series = series[start:]
+    time_off = series[0][0]
+    print 'time\tvolume\tdelta'
+    for i in range(len(series)):
+        t, v = series[i]
+        if i == 0:
+            delta = 0
+        else:
+            pt, pv = series[i-1]
+            delta = (v-pv)/float(t-pt)
+        print '%s\t%s\t%s' % (t - time_off, v, delta)
 
 if __name__ == '__main__':
     exit(main())
