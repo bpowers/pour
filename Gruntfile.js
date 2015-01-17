f000ced2004cb4d9a84070022005dfb762ed0864
Bobby Powers <bobbypowers@gmail.com>
2015-01-17 17:18:39 -0500

    grunt: inline scripts in vulcanize

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,36 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    watch: {
-      default: {
-        options: {
-          spawn: false,
-          interrupt: true
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:package.json>',
+        watch: {
+            default: {
+                options: {
+                    spawn: false,
+                    interrupt: true
+                },
+                files: ['www/bower_components/**/*.html', 'www/index.html'],
+                tasks: ['vulcanize']
+            }
         },
-        files: ['www/bower_components/**/*.html', 'www/index.html'],
-        tasks: ['vulcanize']
-      }
-    },
-    vulcanize: {
-      default: {
-        options: {
-          csp: true,
-          strip: true
-        },
-        files: {
-          'www/build.html': 'www/index.html'
+        vulcanize: {
+            default: {
+                options: {
+                    csp: true,
+                    inline: true,
+                    strip: true
+                },
+                files: {
+                    'www/build.html': 'www/index.html'
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-vulcanize');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-vulcanize');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['vulcanize', 'watch']);
+    // Default task.
+    grunt.registerTask('default', ['vulcanize', 'watch']);
 
 };
